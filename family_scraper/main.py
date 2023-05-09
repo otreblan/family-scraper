f7ab7bb471d77f8a300310ef9ddfb9694bd1e940
otreblan <otreblain@gmail.com>
2023-05-09 18:47:53 -0500

    feat(main): Add argument parser

--- family_scraper/main.py
+++ family_scraper/main.py
@@ -1,6 +1,13 @@
+import argparse
 from selenium import webdriver
 
 def main():
+    parser = argparse.ArgumentParser(description="Scraper")
+
+    parser.add_argument("url", metavar="URL", type=str)
+
+    args = parser.parse_args()
+
     driver = webdriver.Firefox()
 
-    driver.get("https://google.com")
+    driver.get(args.url)
